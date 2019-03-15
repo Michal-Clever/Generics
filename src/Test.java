632a6886f528c529940dd5ed3b7f2e858f11c31f
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-03-15 17:12:20 +0100

    Generics

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,6 @@
+import java.security.KeyPair;
+import java.util.Optional;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -5,9 +8,11 @@ public class Test {
         String fromBox = box.getFromBox();
         System.out.println(fromBox);
 
-        Person person = new Person("Andrzej","Golota");
+        Person person = new Person("Andrzej", "Golota");
         Box<Person> room = new Box<>(person);
         Person someone = room.getFromBox();
         System.out.println(someone);
+
+        Optional<String> optional = new Optional<>();
     }
 }
