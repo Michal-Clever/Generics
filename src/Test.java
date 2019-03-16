0b175f9c7e534fcadbde2c1cec0a6bafb621ffd3
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-03-16 12:22:43 +0100

    Generics + Optional

--- src/Test.java
+++ src/Test.java
@@ -4,22 +4,28 @@ import java.util.Optional;
 public class Test {
     public static void main(String[] args) {
 
-        Box<String> box = new Box<>("roleks");
-        String fromBox = box.getFromBox();
-        System.out.println(fromBox);
-
-        Person person = new Person("Andrzej", "Golota");
-        Box<Person> room = new Box<>(person);
-        Person someone = room.getFromBox();
-        System.out.println(someone);
+        boxExemple();
+        javaExample();
 
 
+    }
+
+    private static void javaExample() {
         Optional<Double> divideResult = divide(1.0, 0.0);
         if (divideResult.isPresent()) {
             System.out.println(divideResult.get());
         } else {
             System.out.println("Division by zero");
         }
+    }
+
+    private static void boxExemple() {
+        Box<String> box = new Box<>("roleks");
+        String fromBox = box.getFromBox();
+
+        Person person = new Person("Andrzej", "Golota");
+        Box<Person> room = new Box<>(person);
+        Person someone = room.getFromBox();
     }
 
     private static Optional<Double> divide(double a, double b) {
